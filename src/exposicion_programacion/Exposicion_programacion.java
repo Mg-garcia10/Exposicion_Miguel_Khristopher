1630bb9b750287ba7c8bac41eeee149add0f976b
khristopherrru2006 <khristopherr22@gmail.com>
2024-09-01 19:01:23 -0600

    Primer Commit Khristopher

--- src/exposicion_programacion/Exposicion_programacion.java
+++ src/exposicion_programacion/Exposicion_programacion.java
@@ -4,6 +4,8 @@
  */
 package exposicion_programacion;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Miguel
@@ -15,6 +17,35 @@ public class Exposicion_programacion {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner lea = new Scanner (System.in);
+        
+        System.out.println("------------BIENVENIDO-------------");
+        System.out.println("Ingrese la información de la mascota:");
+
+        System.out.print("Especie de la mascota: ");
+        String especieMascota = lea.nextLine();
+
+        System.out.print("Raza de la mascota: ");
+        String razaMascota = lea.nextLine();
+        
+        System.out.print("Nombre de la mascota: ");
+        String nombreMascota = lea.nextLine();
+
+        System.out.print("Edad de la mascota en Años: ");
+        int edadMascota = lea.nextInt();
+
+        System.out.print("Peso de la mascota (en kg): ");
+        double pesoMascota = lea.nextDouble();
+        // Instancia de la clase Mascota 
+        Mascota mascota = new Mascota(especieMascota, razaMascota, nombreMascota, edadMascota, pesoMascota);
+        //Instancia de la clase Veterinaria donde sale informacion del local
+        Veterinaria veterinaria = new Veterinaria ("Pet Plaza,","Col. Las minitas, Calle Venezia #2467,, Av. Juan Lindo, Tegucigalpa",
+                                                    "9695-1456","Abierto las 24 horas", "****");
+        //Impresion del metodo toString utilizado en la clase Mascota 
+        System.out.println(mascota.toString());
+        //Imprimimos el metodo Informacion utilizado en la clase Veterinaria 
+        veterinaria.Informacion();
     }
+}   
     
-}
+    
